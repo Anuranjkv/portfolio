aad7b1598091e95641b2a30ac9e908d679e0a25d
Anuranj <anuranj.git@gmail.com>
2026-03-18 01:41:41 +0530

    day1 main page connected

--- main/views.py
+++ main/views.py
@@ -3,6 +3,5 @@ from django.shortcuts import render
 
 
 # Create your views here.
-render homepage:
 def main_view(request):
     return render(request, 'main.html')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-fom main import views as main_views
+from main import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
